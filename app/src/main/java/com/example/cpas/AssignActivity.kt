6114a33dd935585e6147b8772e2aba61c28d954a
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2022-03-05 20:13:48 +0900

    임시 firebase
    파이어베이스 임시

--- app/src/main/java/com/example/cpas/AssignActivity.kt
+++ app/src/main/java/com/example/cpas/AssignActivity.kt
@@ -5,11 +5,16 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.EditText
 import androidx.appcompat.widget.AppCompatButton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
 
 class AssignActivity : AppCompatActivity() {
+    private var auth = FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_assign)
+        val database : FirebaseDatabase = FirebaseDatabase.getInstance()
 
         val assignbtn = findViewById<AppCompatButton>(R.id.Assignbtn)
         val assname = findViewById<EditText>(R.id.AssName)
