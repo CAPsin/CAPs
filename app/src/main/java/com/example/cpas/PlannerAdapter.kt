a445f65253d33edeb70e1095cc5c8dcee2632eb3
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2022-03-31 02:31:41 +0900

    update planner

--- app/src/main/java/com/example/cpas/PlannerAdapter.kt
+++ app/src/main/java/com/example/cpas/PlannerAdapter.kt
@@ -27,8 +27,6 @@ import kotlin.coroutines.CoroutineContext
 
 class PlannerAdapter(private val context: Context, private val plannerActivity: PlannerActivity) :
     RecyclerView.Adapter<PlannerAdapter.CustomViewHolder>() {
-    private lateinit var auth: FirebaseAuth // 파이어 베이스 형식
-    private lateinit var databaseReference: DatabaseReference
     var planList = mutableListOf<String>()
 
     inner class CustomViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -40,56 +38,17 @@ class PlannerAdapter(private val context: Context, private val plannerActivity: 
         viewType: Int,
     ): PlannerAdapter.CustomViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.plan_item, parent, false)
-        auth = FirebaseAuth.getInstance()
-        databaseReference =
-            FirebaseDatabase.getInstance().getReference("Users").child(auth.uid.toString())
         return CustomViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: PlannerAdapter.CustomViewHolder, position: Int) {
         holder.button.text = planList[position]
-        var dateString: String = ""
-        var plantitle: String = ""
-        var flag: Boolean = false
-
-
         holder.itemView.setOnClickListener {
             Log.d("Tag", planList[position] + planList.size)
             if (position == planList.size - 1) {
-                val cal = Calendar.getInstance()    //캘린더뷰 만들기
-                val dateSetListener =
-                    DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
-                        dateString = "${year}년 ${month + 1}월 ${dayOfMonth}일"
-                        Log.d("TAG", dateString) //날짜 선택 후 확인 눌렀을 때 Dialog 나오도록 함
-                        val alert = AlertDialog.Builder(context) // 임시 명준이 dialog
-                        alert.setTitle("할 일 추가")
-                        val edittext = EditText(context)
-
-                        alert.setPositiveButton("추가") { dialog, which ->
-                            plantitle = edittext.text.toString()
-                            planList.add(0, edittext.text.toString())
-                            flag = true
-                            notifyDataSetChanged()
-                            val data = planData(plantitle, "0")
-                            plannerActivity.upload(data)
-//                            databaseReference.child("plan").child(dateString).setValue(data)
-                            //done.await()
-                        }
-                        alert.setNeutralButton("취소", null)
-
-                        alert.setView(edittext)
-                        alert.create()
-                        alert.show()
-                    }
-                DatePickerDialog(context,
-                    dateSetListener,
-                    cal.get(Calendar.YEAR),
-                    cal.get(Calendar.MONTH),
-                    cal.get(Calendar.DAY_OF_MONTH)).show()
-            } else {
-                return@setOnClickListener
+                val intent = Intent(context, SetPlanActivity::class.java)
+                ActivityCompat.startActivityForResult(holder.itemView.context as Activity,intent,101,null)
             }
-
         }
     }
 
