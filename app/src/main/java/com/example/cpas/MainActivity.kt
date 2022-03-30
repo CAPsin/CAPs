721f153ac7a49dea2dfa054a99484bf745eb9202
kmj0973 <kmj0973@naver.com>
2022-03-31 04:27:20 +0900

    update categoryshape

--- app/src/main/java/com/example/cpas/MainActivity.kt
+++ app/src/main/java/com/example/cpas/MainActivity.kt
@@ -68,13 +68,18 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 category_scollview.visibility = View.VISIBLE
                 category_scollview.startAnimation(scaleDownAnim)
             }
-            }
+            isUp = !isUp
+        }
 
         more.setOnClickListener {
             val drawer : DrawerLayout = findViewById(R.id.layout_drawer)
             drawer.openDrawer(GravityCompat.END)
         }
         category_plus.setOnClickListener { //카테고리 추가 버튼
+            val layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
             val dialog = CategoryDialog(this) // 카테고리 다이얼로그 액티비티 받기
             val category_text = TextView(this)
             dialog.showDialog()
@@ -82,8 +87,11 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 override fun onClicked(name: String)
                 {
                     category_text.text = name
-                    category_text.setTextSize(10F)
+                    category_text.setTextSize(20F)
+                    category_text.layoutParams = layoutParams
+                    layoutParams.setMargins(changeDP(5), changeDP(12), changeDP(5), 0)
                     category_text.setTextColor(Color.BLACK)
+                    //category_text.setCompoundDrawables(R.drawable.is_category_bolder,null,null,null)
                     category_line.addView(category_text)
                 }
 
@@ -113,7 +121,11 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         viewPager.adapter = pageradapter
         tabLayout.setupWithViewPager(viewPager)
     }
-
+    private fun changeDP(value : Int) : Int{
+        var displayMetrics = resources.displayMetrics
+        var dp = Math.round(value * displayMetrics.density)
+        return dp
+    }
     private fun setFrag(fragNum : Int) {
         val ft = supportFragmentManager.beginTransaction()
         when(fragNum) {
