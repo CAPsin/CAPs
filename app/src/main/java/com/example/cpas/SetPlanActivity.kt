3dd2fccb1ede1f16258802ac57b75c7b749fbfbb
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2022-03-31 13:23:26 +0900

    cc
    asdd

--- app/src/main/java/com/example/cpas/SetPlanActivity.kt
+++ app/src/main/java/com/example/cpas/SetPlanActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
 import android.util.Log
 import android.widget.DatePicker
 import android.widget.EditText
@@ -36,52 +38,52 @@ class SetPlanActivity : AppCompatActivity() {
 
 
         var dateString = ""
-        val cal : DatePicker = findViewById(R.id.datepicker)
-        val editplan : EditText = findViewById(R.id.editplan)
-        val checkButton : ImageButton = findViewById(R.id.check_button)
-        val cancleButton : ImageButton = findViewById(R.id.cancel_button)
+        val cal: DatePicker = findViewById(R.id.datepicker)
+        val editplan: EditText = findViewById(R.id.editplan)
+        val checkButton: ImageButton = findViewById(R.id.check_button)
+        val cancleButton: ImageButton = findViewById(R.id.cancel_button)
         val sdf = SimpleDateFormat("yyyy/MM/dd")
-        val time : String = sdf.format(Date())
+        val time: String = sdf.format(Date())
         val nowdate = time.split("/")
         dateString = "${nowdate[0]}년 ${nowdate[1]}월 ${nowdate[2]}일"
         cal.setOnDateChangedListener { datePicker, year, month, day ->
-            if(month + 1 < 10 && day < 10){
+            if (month + 1 < 10 && day < 10) {
                 dateString = "${year}년 0${month + 1}월 0${day}일"
-            }
-            else if(month + 1 < 10){
+            } else if (month + 1 < 10) {
                 dateString = "${year}년 0${month + 1}월 ${day}일"
-            }
-            else if(day < 10){
+            } else if (day < 10) {
                 dateString = "${year}년 ${month + 1}월 0${day}일"
-            }
-            else{
+            } else {
                 dateString = "${year}년 ${month + 1}월 ${day}일"
             }
             Log.d("TAG", dateString) //날짜 선택 후 확인 눌렀을 때 Dialog 나오도록 함
         }
 
         checkButton.setOnClickListener {
-            if(editplan.equals("")) {
+            if (editplan.text.toString().equals("")) {
                 Toast.makeText(this, "할 일을 입력해주세요!", Toast.LENGTH_SHORT).show()
-            }
-            else {
-                //val data = planData(editplan.text.toString(), "0")
+            } else {
                 val data = mapOf<String, String>(
                     "title" to editplan.text.toString(),
                     "time" to "0"
                 )
-                
-                databaseReference.child("plan").child(dateString).child(editplan.text.toString()).updateChildren(data).addOnSuccessListener {
+
+
+                databaseReference.child("plan").child(dateString).child(editplan.text.toString())
+                    .updateChildren(data).addOnSuccessListener {
                     val intent = Intent(this, PlannerActivity::class.java)
                     intent.putExtra("nowdate", dateString)
                     startActivity(intent)
                     flag = true
-                    finish()
-                }.addOnFailureListener{
+
+                }.addOnFailureListener {
                     Toast.makeText(this, "실패", Toast.LENGTH_SHORT).show()
                 }
 
             }
         }
+        Handler().postDelayed({
+            Log.d("TAG", "몰루")
+        }, 2000)
     }
 }
