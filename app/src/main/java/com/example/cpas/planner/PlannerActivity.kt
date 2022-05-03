b70e901a9daae24f2203404047b92c8cf0fb9337
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2022-05-04 02:41:08 +0900

    update planner
    기능 업데이트

--- app/src/main/java/com/example/cpas/planner/PlannerActivity.kt
+++ app/src/main/java/com/example/cpas/planner/PlannerActivity.kt
@@ -1,10 +1,11 @@
 package com.example.cpas.planner
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageButton
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cpas.R
 import com.google.firebase.auth.FirebaseAuth
@@ -12,8 +13,12 @@ import com.google.firebase.database.*
 import java.text.SimpleDateFormat
 import java.util.*
 
+
 class PlannerActivity : AppCompatActivity() {
     var dateString = ""
+    var year = 0
+    var month = 0
+    var day = 0
     lateinit var planAdapter : PlannerAdapter
     var planDataList = mutableListOf<String>()
     private lateinit var auth: FirebaseAuth // 파이어 베이스 형식
@@ -30,6 +35,9 @@ class PlannerActivity : AppCompatActivity() {
         val time : String = sdf.format(Date())
         val nowdates = time.split("/")
         var nowdate = "${nowdates[0]}년 ${nowdates[1]}월 ${nowdates[2]}일"
+        year = nowdates[0].toInt()
+        month = nowdates[1].toInt() - 1
+        day = nowdates[2].toInt()
         pickdate.text = nowdate
         auth = FirebaseAuth.getInstance()
         databaseReference = FirebaseDatabase.getInstance().getReference("Plan").child(id.toString())
@@ -54,6 +62,7 @@ class PlannerActivity : AppCompatActivity() {
                 planDataList.clear()
                 for(data in snapshot.children){
                     planDataList.add(data.child("title").value.toString())
+                    Log.d("TAg", planDataList.toString())
                 }
                 planDataList.add("+")
                 planListView.adapter?.notifyDataSetChanged()
@@ -66,9 +75,43 @@ class PlannerActivity : AppCompatActivity() {
         })
 
         calendarButton.setOnClickListener {
-            Log.d("TAG", "임시 달력 리스너")
-            databaseReference.child("plan").child(dateString).child("test")
-                .updateChildren(data)
+            val cal = Calendar.getInstance() // 캘린더 뷰
+            val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                if (month + 1 < 10 && dayOfMonth < 10) {
+                    dateString = "${year}년 0${month + 1}월 0${dayOfMonth}일"
+                } else if (month + 1 < 10) {
+                    dateString = "${year}년 0${month + 1}월 ${dayOfMonth}일"
+                } else if (dayOfMonth < 10) {
+                    dateString = "${year}년 ${month + 1}월 0${dayOfMonth}일"
+                } else {
+                    dateString = "${year}년 ${month + 1}월 ${dayOfMonth}일"
+                }
+                this.year = year
+                this.month = month
+                day = dayOfMonth
+                 Log.d("TAG","날짜 : "+dateString)
+                nowdate = dateString
+                pickdate.text = nowdate
+
+                databaseReference.child(nowdate).addValueEventListener(object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        planDataList.clear()
+                        for(data in snapshot.children){
+                            planDataList.add(data.child("title").value.toString())
+                            Log.d("TAg", planDataList.toString())
+                        }
+                        planDataList.add("+")
+                        planListView.adapter?.notifyDataSetChanged()
+
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                    }
+
+                })
+
+            }
+            DatePickerDialog(this, dateSetListener, year,month,day).show()
         }
     }
 
